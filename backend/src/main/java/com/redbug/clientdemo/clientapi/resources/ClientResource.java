c34f1270e16f5ad464103c094c260bb4c31efb2b
BugRed <o.adrian.rodrigues@gmail.com>
2023-10-24 17:07:59 -0300

    created findByid clients

--- backend/src/main/java/com/redbug/clientdemo/clientapi/resources/ClientResource.java
+++ backend/src/main/java/com/redbug/clientdemo/clientapi/resources/ClientResource.java
@@ -5,10 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.redbug.clientdemo.clientapi.entities.Client;
+import com.redbug.clientdemo.clientapi.dto.ClientDTO;
 import com.redbug.clientdemo.clientapi.services.ClientService;
 
 @RestController
@@ -20,8 +21,15 @@ public class ClientResource {
     
 
     @GetMapping
-    public ResponseEntity<List<Client>> findAll(){
-        List<Client> list = service.findAll();
+    public ResponseEntity<List<ClientDTO>> findAll(){
+        List<ClientDTO> list = service.findAll();
         return ResponseEntity.ok().body(list);
+    }
+    
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+        ClientDTO dto = service.findById(id);
+        return ResponseEntity.ok().body(dto);
     }
 }
--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,5 +1,11 @@
 
 
-INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Jeferson Rodriguez', '152478964', 1000.12, 0, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z');
-INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Dalai Brother', '1547852157', 555.78, 5, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z');
-INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Maraia Pulitzer', '87456215478', 1234.12, 1, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z');
+INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Jeferson Rodriguez', '152478964', 1000.12, 0, TIMESTAMP WITH TIME ZONE '2021-10-13T04:50:07.12345Z');
+INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Dalai Brother', '1547852157', 555.78, 5, TIMESTAMP WITH TIME ZONE '2019-07-12T20:50:59Z');
+INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Maraia Pulitzer', '87575755478', 1234.12, 1, TIMESTAMP WITH TIME ZONE '2015-10-15T20:50:55');
+INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Pablo Anines', '2478964477', 5000.40, 3, TIMESTAMP WITH TIME ZONE '2012-08-13T20:50:01Z');
+INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Kamaitach Guerro', '15474545457', 555.78, 2, TIMESTAMP WITH TIME ZONE '2023-12-13T20:50:22Z');
+INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Mia Polansky', '85757575478', 1234.12, 2, TIMESTAMP WITH TIME ZONE '2012-08-13T20:50:13Z');
+INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Juvenir Arnaldo', '1575757757574', 1000.12, 0, TIMESTAMP WITH TIME ZONE '2010-04-13T10:08:00Z');
+INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Jiggles, the Bell', '15474545775', 555.78, 5, TIMESTAMP WITH TIME ZONE '2007-12-13T20:50:42Z');
+INSERT INTO tb_client(name, CPF, income, children, birth_date) VALUES ('Larissa Manoela', '454242111478', 1234.12, 0, TIMESTAMP WITH TIME ZONE '2000-12-13T20:50:55Z');
